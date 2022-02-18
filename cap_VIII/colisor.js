d3d7bbfdbd161aeb39e92e442c670dc697b183f1
Ebenezer Dorneles <ebenezerdorneles@gmail.com>
2022-02-18 15:35:45 -0300

    🐛 Correção do bug de vida infinita

--- cap_VIII/colisor.js
+++ cap_VIII/colisor.js
@@ -1,5 +1,6 @@
 function Colisor() {
   this.sprites = [];
+  this.aoColidir = null; 
   this.spritesExcluir = [];
 }
 
@@ -11,7 +12,7 @@ Colisor.prototype = {
 
   processar: function () {
     //inicio com o objeto vazio
-    const jaTestados = new Object();
+    var jaTestados = new Object();
 
     for (let i in this.sprites) {
       for (let j in this.sprites) {
@@ -19,20 +20,15 @@ Colisor.prototype = {
         if (i == j) continue;
 
         //gerar strings unicas
-        const id1 = this.stringUnica(this.sprites[i]);
-        const id2 = this.stringUnica(this.sprites[j]);
+       let id1 = this.stringUnica(this.sprites[i]);
+       let id2 = this.stringUnica(this.sprites[j]);
 
         //criar os arrays se não existirem
         if (!jaTestados[id1]) jaTestados[id1] = [];
         if (!jaTestados[id2]) jaTestados[id2] = [];
 
         //teste de repetição
-        if (
-          !(
-            jaTestados[id1].indexOf(id2) >= 0 ||
-            jaTestados[id2].indexOf(id1) >= 0
-          )
-        ) {
+        if (!(jaTestados[id1].indexOf(id2) >= 0 || jaTestados[id2].indexOf(id1) >= 0)) {
           //abstrair a colisão
           this.testarColisao(this.sprites[i], this.sprites[j]);
           //registrando o teste
@@ -47,8 +43,9 @@ Colisor.prototype = {
 
   testarColisao: function (sprite1, sprite2) {
     //Obter retângulos de colisão de cada sprite
-    const rests1 = sprite1.retangulosColisao();
-    const rests2 = sprite2.retangulosColisao();
+    let rests1 = sprite1.retangulosColisao();
+    let rests2 = sprite2.retangulosColisao();
+    var colidiu = false;
 
     //testar as colisões
     colisoes: for (let i in rests1) {
@@ -68,13 +65,11 @@ Colisor.prototype = {
 
   retangulosColidem: function (ret1, ret2) {
     //interecção entre triângulos
-    return (
-      ret1.x + ret1.largura > ret2.x &&
-      ret1.x < ret2.x + ret2.largura &&
-      ret1.y + ret1.altura > ret2.y &&
-      ret1.y < ret2.y + ret2.altura
-    );
-  },
+      return (ret1.x + ret1.largura) > ret2.x &&
+             ret1.x < (ret2.x + ret2.largura) &&
+             (ret1.y + ret1.altura) > ret2.y &&
+             ret1.y < (ret2.y + ret2.altura);
+   },
 
   stringUnica: function (sprite) {
     var str = "";
@@ -112,7 +107,7 @@ Colisor.prototype = {
     }
 
     //limpar new_array
-    this.new_array = [];
+    this.spritesExcluir = [];
 
     //subtituir o velho pelo novo;
     this.sprites = new_array;
